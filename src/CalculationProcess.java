e45202267c594bb1b742dcd9c5e9baa9acf31901
Sam <samuelfeindt@gmail.com>
2024-12-06 10:51:29 +0100

    Update2

--- src/CalculationProcess.java
+++ src/CalculationProcess.java
@@ -1,3 +1,42 @@
 public class CalculationProcess {
-    
+/*
+    public int calculateFAZ(Networkplan networkplan) {
+        int maxFEZ = 0;
+
+        for (Process predecessor : networkplan.getListOfProcesses()) {
+            if (predecessor.getFaz() > maxFEZ) {
+                maxFEZ = predecessor.getFez();
+            }
+        }
+        return maxFEZ;
+    }
+
+    public int calculateFEZ(Process process) {
+        return process.getFaz() + process.getDuration();
+    }
+
+    public int calculateSAZ(Process process) {
+        return process.getSez() - process.getDuration();
+    }
+
+    public int calculateSEZ(Process process) {
+        if (process.successor.isEmpty()) {
+            return process.getFez();
+        }
+
+        int minSAZ = Integer.MAX_VALUE;
+
+        for (Process successor : process.successor) {
+            if (successor.getSaz() < minSAZ) {
+                minSAZ = successor.getSaz();
+            }
+        }
+    }
+
+    public int calculateTotalFloat(Process process) {
+
+    }
+
+ */
+
 }
--- src/Process.java
+++ src/Process.java
@@ -1,5 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Process {
 
+    private String name;
+    private int duration;
+    private int faz, fez, saz, sez, freierPuffer, gesamtPuffer;
 
+    // Konstruktor
+    public Process(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getFaz() {
+        return faz;
+    }
+
+    public void setFaz(int faz) {
+        this.faz = faz;
+    }
+
+    public int getFez() {
+        return fez;
+    }
+
+    public void setFez(int fez) {
+        this.fez = fez;
+    }
+
+    public int getSaz() {
+        return saz;
+    }
+
+    public void setSaz(int saz) {
+        this.saz = saz;
+    }
+
+    public int getSez() {
+        return sez;
+    }
+
+    public void setSez(int sez) {
+        this.sez = sez;
+    }
+
+    public int getFreierPuffer() {
+        return freierPuffer;
+    }
+
+    public void setFreierPuffer(int freierPuffer) {
+        this.freierPuffer = freierPuffer;
+    }
+
+    public int getGesamtPuffer() {
+        return gesamtPuffer;
+    }
+
+    public void setGesamtPuffer(int gesamtPuffer) {
+        this.gesamtPuffer = gesamtPuffer;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name;
+    }
 }
